fa2af08460386b0ac6ac25c2fd9e5ffb3c225640
Onting <ontingleung@tutamail.com>
2024-10-25 16:05:06 +0100

    quaterions

--- games-engines-2024/scenes/enemy.gd
+++ games-engines-2024/scenes/enemy.gd
@@ -2,7 +2,29 @@ extends MeshInstance3D
 
 @onready var tank:Node3D = $"../tank"
 
+var to_player
+var forw
+var axis
+var theta
+
+var q1:Quaternion
+var q2:Quaternion
+var t
 func _ready() -> void:
+	
+	to_player = global_position - tank.global_position 
+	to_player = to_player.normalized()
+	forw = global_transform.basis.z
+	
+	DebugDraw3D.draw_arrow(global_position, global_position + forw * 5, Color.AQUA, 0.1)
+	DebugDraw2D.set_text("enemy_to_player", to_player)
+	DebugDraw2D.set_text("forw", forw)
+	axis = to_player.cross(forw)
+	theta = acos(to_player.dot(forw))
+	
+	q2 = Quaternion(axis, theta)
+	q1 = global_basis.get_rotation_quaternion()
+	t = 0
 	
 
 	
@@ -11,17 +33,20 @@ func _ready() -> void:
 	
 func _process(delta: float) -> void:
 	
-	var to_player = tank.global_position - global_position
+	var q:Quaternion
 	
-	var forw = global_transform.basis.z
-	
-	DebugDraw3D.draw_arrow(global_position, global_position + forw * 5, Color.AQUA, 0.1)
-	DebugDraw2D.set_text("enemy_to_player", to_player)
-	DebugDraw2D.set_text("forw", forw)
-	
-	var axis = to_player.cross(forw)
-	
-	DebugDraw2D.set_text("axis", axis)
-	
+	if (t < 1.0):
+		t = t + delta
+		q = q1.slerp(q2, t)
+		global_basis = Basis(q)
+		rotation = Vector3(0, t, 0)
+
+	else:
+		q = q1.slerp(q2, t)
+		global_basis = Basis(q)
+		rotation = Vector3(0, t, 0)
+		
+		
+
 
 	pass
