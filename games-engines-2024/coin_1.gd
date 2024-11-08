9f0a9dd81f9f0cf36d5c03400bff650d064053df
Onting <ontingleung@tutamail.com>
2024-11-08 23:08:52 +0000

    apce

--- games-engines-2024/coin_1.gd
+++ games-engines-2024/coin_1.gd
@@ -1,1 +1,16 @@
 extends CSGCylinder3D
+
+@export var velocity:Vector3
+@export var gravity:Vector3 = Vector3(0, -9.8, 0)
+@export var time:float = 0
+
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if global_position.y > 0:
+		time += delta
+		velocity += gravity * delta
+		global_position += velocity * delta
+	pass
