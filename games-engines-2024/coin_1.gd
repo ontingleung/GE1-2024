876ad90d639b067f134f5d7d367319bce80f6a55
Onting <ontingleung@tutamail.com>
2024-12-05 15:33:41 +0000

    spa

--- games-engines-2024/coin_1.gd
+++ games-engines-2024/coin_1.gd
@@ -12,6 +12,8 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if global_position.y > 0:
